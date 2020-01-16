0548699e8c41dfc4dcd97f489566085fcb3a1a88
Zack Amin <ishaqhamin@gmail.com>
2020-01-16 16:18:38 +0000

    multipleVariablesInts

--- src/variables.java
+++ src/variables.java
@@ -2,11 +2,12 @@ public class variables {
 
     public static void main (String[] args) {
 
-        int myFirstNumber = 5;
-        int mySecondNumber = (10 + 5) + (2 * 10);
+        int myFirstNumber = (10 + 5) + (2 * 10);
+        int mySecondNumber = 12;
+        int myThirdNumber = 6;
+        int myTotal = myFirstNumber + mySecondNumber + myThirdNumber;
 
-        System.out.println(mySecondNumber);
-        System.out.println(myFirstNumber);
+        System.out.println(myTotal);
     }
 
 }
