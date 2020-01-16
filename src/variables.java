bcfd1bf3b7639cf9366d66813070710ca3390d23
Zack Amin <ishaqhamin@gmail.com>
2020-01-16 15:01:19 +0000

    variableoperators

--- src/variables.java
+++ src/variables.java
@@ -1,8 +1,11 @@
 public class variables {
 
     public static void main (String[] args) {
-        int myFirstNumber = 5;
 
+        int myFirstNumber = 5;
+        int mySecondNumber = (10 + 5) + (2 * 10);
+
+        System.out.println(mySecondNumber);
         System.out.println(myFirstNumber);
     }
 
